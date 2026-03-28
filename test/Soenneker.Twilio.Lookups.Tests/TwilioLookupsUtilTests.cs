8ac2344b9b3364f2d7060e703f5deb9a66a574dd
Jake Soenneker <jake@soenneker.com>
2026-03-27 20:33:17 -0500

    misc

--- test/Soenneker.Twilio.Lookups.Tests/TwilioLookupsUtilTests.cs
+++ test/Soenneker.Twilio.Lookups.Tests/TwilioLookupsUtilTests.cs
@@ -1,6 +1,8 @@
 using Soenneker.Twilio.Lookups.Abstract;
 using Soenneker.Tests.FixturedUnit;
 using System.Threading.Tasks;
+using Soenneker.Facts.Local;
+using Soenneker.Twilio.OpenApiClient.Models;
 using Xunit;
 
 namespace Soenneker.Twilio.Lookups.Tests;
@@ -8,6 +10,9 @@ namespace Soenneker.Twilio.Lookups.Tests;
 [Collection("Collection")]
 public sealed class TwilioLookupsUtilTests : FixturedUnitTest
 {
+    private const string _phoneNumber = "";
+    private const string _nomoroboSpamScore = "";
+
     private readonly ITwilioLookupsUtil _util;
 
     public TwilioLookupsUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
@@ -17,6 +22,16 @@ public sealed class TwilioLookupsUtilTests : FixturedUnitTest
 
     [Fact]
     public void Default()
-    { 
+    {
+    }
+
+    [LocalFact]
+    public async Task GetPhoneNumber_should_return_lookup_data()
+    {
+        Lookups_v1_phone_number? result = await _util.GetPhoneNumber(_phoneNumber, addOns: [_nomoroboSpamScore], cancellationToken: CancellationToken);
+
+        Assert.NotNull(result);
+        Assert.Equal(_phoneNumber, result!.PhoneNumber);
+        Assert.NotNull(result.AddOns);
     }
 }
